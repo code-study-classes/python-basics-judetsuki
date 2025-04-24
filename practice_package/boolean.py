ecddd33a68cba9cf1ce08d37227b68634d440471
judetsuki <Luka9609@gmail.com>
2025-04-25 01:56:13 +0300

    boolean completed

---
+++ practice_package/boolean.py
@@ -0,0 +1,13 @@
+check_between_numbers = lambda a, b, c: ((b > a) and (b < c)) or ((b < a) and (b > c))
+
+check_odd_three = lambda num: (len(str(abs(num))) == 3) and (num % 2 != 0)
+
+
+check_unique_digits =  lambda num: (len(set(str(abs(num)))) == 3)
+
+
+def check_palindrome_number(num):
+        num_str = str(abs(num))
+        return num_str == num_str[::-1]
+
+check_ascending_digits = lambda num: (len(str(abs(num))) == 3) and (abs(num) // 100 < (abs(num) // 10) % 10 < abs(num) % 10)
